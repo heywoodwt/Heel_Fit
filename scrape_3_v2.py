f555ac9015a4af92fe31e0fd9f31df3380d085dd
Heywood Williams-Tracy <112906201+heywoodwt@users.noreply.github.com>
2023-06-24 17:14:10 -0400

    Update scrape_3_v2.py

--- scrape_3_v2.py
+++ scrape_3_v2.py
@@ -5,15 +5,15 @@ import time
 from twilio.rest import Client
 
 # Twilio account details
-account_sid = "AC7b2882f87de24be321f1efc9d9d8c8a5"
-auth_token = "d64c8116240ac394404c7bf059adffbe"
+account_sid = "ACCOUNT SID"
+auth_token = "AUTH TOKEN"
 client = Client(account_sid, auth_token)
 
 # Phone numbers
-from_number = '+19193715995'  # Twilio number
-to_number = '+17047800822'  # My number (1 for US)
+from_number = '+10000000000'  # Twilio number
+to_number = '+10000000000'  # My number (1 for US)
 
-url = 'https://kotangorocks.z13.web.core.windows.net/'
+url = 'https://stayactive.unc.edu/Program/GetProgramDetails?courseId=c4fca5f6-4090-48c4-aab4-8f96e4e2f9ec&semesterId=3e42e4c2-088f-469a-8ee7-755849351920'
 filename = 'content.html'
 last_content = None
 
